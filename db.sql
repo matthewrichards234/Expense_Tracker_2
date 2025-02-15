8667de850992291c470b5c7d7fd4ef0b5771d77e
Matthew Richards <matthewrichards234@gmail.com>
2025-02-15 16:44:43 -0500

    update sql

--- db.sql
+++ db.sql
@@ -1,5 +1,17 @@
-CREATE TABLE user (
+CREATE TABLE User (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(15) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Category (
     
+);
+
+CREATE TABLE Transaction (
+
 );
